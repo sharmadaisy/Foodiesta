f3af235c3ac44dc4141c2add1b369408b9d54334
sharmadaisy1998@gmail.com <sharmadaisy1998@gmail.com>
2023-04-09 12:34:42 +0530

    linked with backend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { TagsComponent } from './components/partials/tags/tags.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
-
+import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,6 +29,7 @@ import { NotFoundComponent } from './components/partials/not-found/not-found.com
     BrowserModule,
     AppRoutingModule,
     RatingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -12,15 +13,20 @@ export class HomeComponent implements OnInit {
   foods:Food[] = []
   constructor(private foodService: FoodService,
     activatedRoute: ActivatedRoute) {
+      let foodObservable: Observable<Food[]>;
       activatedRoute.params.subscribe((params) => {
         if(params.searchTerm){
-          this.foods = foodService.getAllFoodsBySearchTerm(params.searchTerm)
+          foodObservable = foodService.getAllFoodsBySearchTerm(params.searchTerm)
         } else if (params.tag) {
-        this.foods = this.foodService.getAllFoodsByTag(params.tag);
-      } else {
-          this.foods = foodService.getAll()
-        }
-      })
+          foodObservable = this.foodService.getAllFoodsByTag(params.tag);
+        } else {
+          foodObservable = foodService.getAll()
+          }
+
+        foodObservable.subscribe((serverFoods) => {
+          this.foods = serverFoods
+        })
+        })
    }
 
   ngOnInit(): void {
