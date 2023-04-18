811ef924d0dc8957102917e617e9da185ec034a2
sharmadaisy1998@gmail.com <sharmadaisy1998@gmail.com>
2023-04-18 14:17:36 +0530

    adding map to checkout page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { CartPageComponent } from './components/pages/cart-page/cart-page.compon
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
 import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
+import { AuthGuard } from './auth/guards/auth.guard';
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'search/:searchTerm', component: HomeComponent},
@@ -14,8 +15,7 @@ const routes: Routes = [
   {path: 'cart-page', component: CartPageComponent},
   {path: 'login', component: LoginPageComponent},
   {path:'register', component: RegisterPageComponent},
-  {path:'checkout', component: CheckoutPageComponent},
-];
+  {path:'checkout', component: CheckoutPageComponent, canActivate:[AuthGuard]},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
