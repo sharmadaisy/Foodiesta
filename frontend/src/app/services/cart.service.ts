f994def4eb44dd80ce4b6b3fb62e6fa0a80660fe
sharmadaisy1998@gmail.com <sharmadaisy1998@gmail.com>
2023-04-18 12:06:55 +0530

    checkout page

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -43,6 +43,10 @@ export class CartService {
     return this.cartSubject.asObservable();
   }
 
+  getCart(): Cart{
+    return this.cartSubject.value;
+  }
+
   private setCarttoLocalStorage():void{
     this.cart.totalPrice = this.cart.items.reduce((prevSum, currentItem) =>
       prevSum + currentItem.price, 0
